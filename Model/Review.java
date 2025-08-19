5a985a11d297b9a00b2211387e35a9e9699ae55b
minhhoang2508 <165027027+minhhoang2508@users.noreply.github.com>
2025-08-19 21:54:19 +0700

    Update Review.java

--- Model/Review.java
+++ Model/Review.java
@@ -1,5 +1,3 @@
-package com.library.model;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -201,3 +199,4 @@ public class Review {
                            id, userId, documentId, rating, helpfulVotes);
     }
 }
+
